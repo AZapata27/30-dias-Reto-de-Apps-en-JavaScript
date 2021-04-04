3c86e569d33b5639b0f7a32b9be1cebe4a8a26d5
Andres Zapata <azapata@uc.edu.co>
2021-04-04 17:36:50 -0500

    Se agrega eventlistener de keydown para detectar tecla presionada

--- Simulador-de-Percusion(Bateria)/main.js
+++ Simulador-de-Percusion(Bateria)/main.js
@@ -1,51 +1,15 @@
 
+window.addEventListener('keydown',(e)=>{
+
+  const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audio);
+
+  if(audio==null) return;
+  audio.currentTime=0;
+  audio.play();
 
 
-const key_A = document.getElementById('key_A');
-const key_S = document.getElementById('key_S');
-const key_D = document.getElementById('key_D');
-const key_F = document.getElementById('key_F');
-const key_G = document.getElementById('key_G');
-const key_H = document.getElementById('key_H');
-const key_J = document.getElementById('key_J');
-const key_K = document.getElementById('key_K');
-const key_L = document.getElementById('key_L');
-
-
-const audio_A = document.getElementById('audio_A');
-const audio_S = document.getElementById('audio_S');
-const audio_D = document.getElementById('audio_D');
-const audio_F = document.getElementById('audio_F');
-const audio_G = document.getElementById('audio_G');
-const audio_H = document.getElementById('audio_H');
-const audio_J = document.getElementById('audio_J');
-const audio_K = document.getElementById('audio_K');
-const audio_L = document.getElementById('audio_L');
-
-
-
-const onClick = (key)=>{
-
-    switch (key) {
-        case 'A':
-            audio_A.play();
-            break;
-    
-        default:
-            break;
-    }
-    
-
-
-}
-
-key_A.addEventListener('click',()=>onClick('A'))
-
-
-
-
-
-
+})
 
 
 

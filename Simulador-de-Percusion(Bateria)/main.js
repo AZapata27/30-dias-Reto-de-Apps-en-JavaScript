f65680643913d9919c25f9a89eb8894dfb7546e2
Andres Zapata <azapata@uc.edu.co>
2021-04-04 19:23:53 -0500

    se agrega responsive y funcion de setimeout

--- Simulador-de-Percusion(Bateria)/main.js
+++ Simulador-de-Percusion(Bateria)/main.js
@@ -1,15 +1,39 @@
 
-window.addEventListener('keydown',(e)=>{
+/* Funcion playAudio(e) recibe evento de keydown, si existe sonido asociado a la key presionada 
+lo reproduce y agrega la classe .playing css para animar la key  */
+window.addEventListener('keydown', playAudio );
+function playAudio(e){   
 
-  const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+    const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const keyElement= document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   
+  
+    if(audio==null) return; // retorna si no existe audio
+    audio.currentTime=0; // rebobina el audio al inicio si es que aun esta reproduciendoce para no esperar a que termine
+    audio.play();
+    keyElement.classList.add('playing'); 
+    setTimeout(()=> keyElement.classList.remove('playing'),100);     
+}
 
-  if(audio==null) return;
-  audio.currentTime=0;
-  audio.play();
+// se selecciona todos lo selectores .key
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach( key=> key.addEventListener('click',playAudioOnClick));
+
+function playAudioOnClick(e){   
 
 
-})
+    const audio= document.querySelector(`audio[data-key="${e.target.dataset.key}"]`);
+    const keyElement= document.querySelector(`.key[data-key="${e.target.dataset.key}"]`);
+   
+  
+    if(audio==null) return; // retorna si no existe audio
+    audio.currentTime=0; // rebobina el audio al inicio si es que aun esta reproduciendoce para no esperar a que termine
+    audio.play();
+    keyElement.classList.add('playing');      
+    setTimeout(()=> keyElement.classList.remove('playing'),100)
+}
 
 
 
